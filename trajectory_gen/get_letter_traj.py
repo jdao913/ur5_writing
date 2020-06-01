b4abd295b2bc5a14c55aec809a7dc86071bbc6fe
baculaa <baculaa@oregonstate.edu>
2020-05-31 20:50:53 -0700

    added scale to getting the letter trajectory

--- trajectory_gen/get_letter_traj.py
+++ trajectory_gen/get_letter_traj.py
@@ -1,14 +1,32 @@
 import numpy as np
 import csv
 
+scale = 0.33
+
 def get_x_y(csv_file):
     x = []
     y = []
     with open(csv_file) as csvfile:
         plots = csv.reader(csvfile,delimiter=',')
         for row in plots:
-            x.append(float(row[0]))
-            y.append(float(row[1]))
+            x.append(float(row[0])*scale)
+            y.append(float(row[1])*scale)
+
+    return x,y
+
+def get_x_y_lower(csv_file):
+    x = []
+    y = []
+    with open(csv_file) as csvfile:
+        plots = csv.reader(csvfile,delimiter=',')
+        i = 0
+        for row in plots:
+            if i%10 == 0:
+                x.append(float(row[0])*scale)
+                y.append(float(row[1])*scale)
+            else:
+                pass
+            i += 1
 
     return x,y
 
@@ -96,82 +114,82 @@ def get_traj(letter):
         xd,yd = get_x_y('capital_letters_csvs/z_cap.csv')
         len_traj = len(xd)
     elif letter == 'a':
-        xd,yd = get_x_y('small_letters_csvs/a_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/a_small.csv')
         len_traj = len(xd)
     elif letter == 'b':
-        xd,yd = get_x_y('small_letters_csvs/b_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/b_small.csv')
         len_traj = len(xd)
     elif letter == 'c':
-        xd,yd = get_x_y('small_letters_csvs/c_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/c_small.csv')
         len_traj = len(xd)
     elif letter == 'd':
-        xd,yd = get_x_y('small_letters_csvs/d_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/d_small.csv')
         len_traj = len(xd)
     elif letter == 'e':
-        xd,yd = get_x_y('small_letters_csvs/e_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/e_small.csv')
         len_traj = len(xd)
     elif letter == 'f':
-        xd,yd = get_x_y('small_letters_csvs/f_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/f_small.csv')
         len_traj = len(xd)
     elif letter == 'g':
-        xd,yd = get_x_y('small_letters_csvs/g_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/g_small.csv')
         len_traj = len(xd)
     elif letter == 'h':
-        xd,yd = get_x_y('small_letters_csvs/h_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/h_small.csv')
         len_traj = len(xd)
     elif letter == 'i':
-        xd,yd = get_x_y('small_letters_csvs/i_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/i_small.csv')
         len_traj = len(xd)
     elif letter == 'j':
-        xd,yd = get_x_y('small_letters_csvs/j_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/j_small.csv')
         len_traj = len(xd)
     elif letter == 'k':
-        xd,yd = get_x_y('small_letters_csvs/k_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/k_small.csv')
         len_traj = len(xd)
     elif letter == 'l':
-        xd,yd = get_x_y('small_letters_csvs/l_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/l_small.csv')
         len_traj = len(xd)
     elif letter == 'm':
-        xd,yd = get_x_y('small_letters_csvs/m_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/m_small.csv')
         len_traj = len(xd)
     elif letter == 'n':
-        xd,yd = get_x_y('small_letters_csvs/n_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/n_small.csv')
         len_traj = len(xd)
     elif letter == 'o':
-        xd,yd = get_x_y('small_letters_csvs/o_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/o_small.csv')
         len_traj = len(xd)
     elif letter == 'p':
-        xd,yd = get_x_y('small_letters_csvs/p_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/p_small.csv')
         len_traj = len(xd)
     elif letter == 'q':
-        xd,yd = get_x_y('small_letters_csvs/q_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/q_small.csv')
         len_traj = len(xd)
     elif letter == 'r':
-        xd,yd = get_x_y('small_letters_csvs/r_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/r_small.csv')
         len_traj = len(xd)
     elif letter == 's':
-        xd,yd = get_x_y('small_letters_csvs/s_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/s_small.csv')
         len_traj = len(xd)
     elif letter == 't':
-        xd,yd = get_x_y('small_letters_csvs/t_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/t_small.csv')
         len_traj = len(xd)
     elif letter == 'u':
-        xd,yd = get_x_y('small_letters_csvs/u_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/u_small.csv')
         len_traj = len(xd)
     elif letter == 'v':
-        xd,yd = get_x_y('small_letters_csvs/v_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/v_small.csv')
         len_traj = len(xd)
     elif letter == 'w':
-        xd,yd = get_x_y('small_letters_csvs/w_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/w_small.csv')
         len_traj = len(xd)
     elif letter == 'x':
-        xd,yd = get_x_y('small_letters_csvs/x_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/x_small.csv')
         len_traj = len(xd)
     elif letter == 'y':
-        xd,yd = get_x_y('small_letters_csvs/y_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/y_small.csv')
         len_traj = len(xd)
     elif letter == 'z':
-        xd,yd = get_x_y('small_letters_csvs/z_small.csv')
+        xd,yd = get_x_y_lower('small_letters_csvs/z_small.csv')
         len_traj = len(xd)
     else:
         xd = []
